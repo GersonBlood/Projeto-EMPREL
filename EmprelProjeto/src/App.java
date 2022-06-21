f3e0ec6087e67eb94494d4df37e298030284e099
Gersonblood <gersonfernandesferreira4@gmail.com>
2022-06-21 17:50:34 -0300

    commit

--- EmprelProjeto/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        byte numeroPequeno = 18;
-
-        System.out.println(numeroPequeno);
-    }
-}
---
+++ EmprelProjeto/src/Complaint.java
@@ -0,0 +1,11 @@
+public class Complaint extends AnimalComplaint; public Complaint() {
+    }
+
+{
+    public static void main(String[] args) throws Exception {
+        if Citizen
+        class.forName()
+        getConnection()
+        System.out.println;
+    }
+}
